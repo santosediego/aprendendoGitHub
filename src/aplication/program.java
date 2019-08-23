f1642270d0bb11efae2bbbf7f18a24852a44c940
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2019-08-23 19:16:03 -0300

    Acrescentada a mensagem de boa noite.

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -15,7 +15,9 @@ public class program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Boa noite!!");
+        
     }
     
 }
